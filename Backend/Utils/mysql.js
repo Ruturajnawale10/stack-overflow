aabdde29d4e973958686433e9dd491502cad37bb
rajat1333 <rajatmasurkar@gmail.com>
2022-05-01 00:50:14 -0700

    updated tags page

--- Backend/Utils/mysql.js
+++ Backend/Utils/mysql.js
@@ -3,7 +3,7 @@ import mysql from 'mysql'
 // var config = require("../configs/config.js")
 import config from "../configs/config.js"
 
-var connPool = mysql.createPool({
+const connPool = mysql.createPool({
   host: config.awsRDS.host,
   user: config.awsRDS.user,
   password: config.awsRDS.password,
@@ -15,6 +15,6 @@ connPool.getConnection(function(err) {
   if (err) throw err;
   console.log("Connected to mysql database!");
 });
-const exports = connPool;
+// const exports = connPool;
 
 export default connPool;

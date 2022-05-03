197fa28d7d23546e990d22025aed3486987a7d68
Abraham Mathew <abrahammathew.dev@gmail.com>
2022-05-02 20:31:58 -0700

    Added sidebar and layout component for main

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -6,22 +6,29 @@ import QuestionsOverview from "../components/QuestionsOverview/QuestionsOverview
 import TagOverview from "../components/Tags/TagOverview.js";
 import QuestionPosting from "../components/QuestionPosting/QuestionPosting.js";
 import AddTag from "./Admin/AddTags";
+import Sidebar from "./Sidebar/Sidebar";
+import LayoutComponent from "./LayoutComponent/LayoutComponent"
 
 class Main extends Component {
     render() {
         return (
             <div>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
-                   <Navbar/>
+                   {/* <Navbar/> */}
+                   {/* <Sidebar/> */}
+                   <LayoutComponent>
                 <Routes>
                     {/* <Route exact path="/" element={<Navbar/>}/> */}
+                    
                     <Route exact path="/login" element={<Login/>}/>
                     <Route path="/questions/:questionID" element={<QuestionsOverview/>}/>
                     <Route path="/questions" element={<QuestionsOverview/>}/>
                     <Route path="/questions/ask" element={<QuestionPosting/>}/>
                     <Route path="/tags" element={<TagOverview/>}/>
                     <Route path="/admin/tags/add" element={<AddTag/>}/>
+                    
                 </Routes>
+                </LayoutComponent>
             </div>
         );
     }

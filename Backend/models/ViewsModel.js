98f086f4b95080f160c64b772357ac4bab9fd1ef
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-05-05 16:19:03 -0700

    - Added three new config keys - Enabled addview of question feature by pushing API request to Kafka only when the useKafka flag is true in config, else makes request to mongo  in nodejs backend server itself.

--- Backend/models/ViewsModel.js
+++ Backend/models/ViewsModel.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 
 var ViewsSchema = new Schema(
   {
-    questionID: { type: String, unique: true },
-    clientIdentity: { type: [String] },
+    clientIdentity: { type: [String], required: true, default: [] },
   },
   {
     versionKey: false,

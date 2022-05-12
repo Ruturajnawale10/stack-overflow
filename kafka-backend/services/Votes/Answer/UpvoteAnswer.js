18c91342c28a1e371afb1ccd41aa58f00963aeff
ruturajnawale10 <ruturajnawale99@gmail.com>
2022-05-11 20:24:57 -0700

    - Added 8 Voting APIs in kafka - Set the expiration of redis cache of 30 minutes

---
+++ kafka-backend/services/Votes/Answer/UpvoteAnswer.js
@@ -0,0 +1,30 @@
+"use strict";
+import Questions from "../../../models/QuestionModel.js";
+import Users from "../../../models/UserModel.js";
+
+async function handle_request(msg) {
+  console.log("In Kafka handle request:" + JSON.stringify(msg));
+  console.log("answer upvote Request");
+  let questionID = msg.questionID;
+  let userID = msg.userID;
+  let answerID = msg.answerID;
+  console.log("Seee". answerID)
+  let answeredByUserID = msg.answeredByUserID;
+
+  Questions.findOneAndUpdate(
+    { _id: questionID, "answers._id": answerID },
+    { $push: { "answers.$.upVotes": userID } },
+    async function (error, question) {
+      if (error) {
+        return;
+      } else {
+        await Users.findOneAndUpdate(
+          { _id: answeredByUserID },
+          { $inc: { reputation: 5 } }
+        );
+      }
+    }
+  );
+}
+
+export default handle_request;
